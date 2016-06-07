1a29653a87c556057cbc51c21cb06e0c07db7f54
Crystal Stellwagen <liraeldianne@gmail.com>
2016-06-06 17:23:54 -0700

    selection sort that swaps the max value too

--- python-track/python/selection-sort.py
+++ python-track/python/selection-sort.py
@@ -4,16 +4,25 @@ from datetime import datetime
 def selection_sort(arr):
     ops = 0
     sortednums = 0
-    while sortednums < len(arr)-1:
+    while sortednums < len(arr)/2:
         mindex = sortednums
-        for i in range(sortednums+1, len(arr)):
+        maxdex = sortednums
+        for i in range(sortednums+1, len(arr)-sortednums):
             ops += 1
             if arr[i] < arr[mindex]:
                 mindex = i
+            if arr[i] > arr[maxdex]:
+                maxdex = i
         arr[sortednums], arr[mindex] = arr[mindex], arr[sortednums]
+
+        if maxdex == sortednums:
+            arr[mindex], arr[len(arr)-1-sortednums] = arr[len(arr)-1-sortednums], arr[mindex]
+        else:
+            arr[maxdex], arr[len(arr)-1-sortednums] = arr[len(arr)-1-sortednums], arr[maxdex]
         sortednums += 1
-    print ops
+    print "ops:", ops
     return arr
+
 
 sample = [random.randrange(10000) for x in range(10000)]
 date1 = datetime.now()
