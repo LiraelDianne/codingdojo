2bd0ddcbd20f6fe245399dad1d950d3a79552d03
Crystal Stellwagen <liraeldianne@gmail.com>
2016-06-14 09:01:07 -0700

    fixed bugs in doubly-linked list, expanded insert and delete methods

--- python-track/oop/doubly-linked-list.py
+++ python-track/oop/doubly-linked-list.py
@@ -9,29 +9,50 @@ class DoublyLinkedList(object):
         self.head = None
         self.tail = None
     def addNode(self, node):
-        if not self.head:
+        if self.head:
+            if self.tail.prev:
+                self.tail.next = node
+                node.prev = self.tail
+                self.tail = node
+            else:
+                self.tail = node
+                self.head.next = node
+                self.tail.prev = self.head
+        else:
             self.head = node
             self.tail = node
-        else:
-            self.tail = node
-            if not self.tail.prev:
-                self.head.next = node
-            else:
-                self.tail.prev = node
 
-    def insertNode(self, node, prev, next):
+    def insertNode(self, node, next):
+        if next.prev:
+            next.prev.next = node
+            node.prev = next.prev
+        else:
+            self.head = node
         node.next = next
-        node.prev = prev
-        node.prev.next = node
-        node.next.prev = node
+        next.prev = node
 
     def deleteNode(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
+        if node.prev:
+            if node.next:
+                node.prev.next = node.next
+                node.next.prev = node.prev
+            else:
+                self.tail = node.prev
+                node.prev.next = None
+        else:
+            if node.next:
+                self.head = node.next
+                node.next.prev = None
+            else:
+                self.head = None
+                self.tail = None
+        node.prev = None
+        node.next = None
 
     def printVals(self):
         if self.head:
-            print head.name
+            print "List:"
+            print self.head.name
             if self.tail is not self.head:
                 current = self.head.next
                 while current is not self.tail:
@@ -47,11 +68,15 @@ carol = Node('carol')
 
 people = DoublyLinkedList()
 people.printVals()
-people.addNode(alice)
-people.printVals()
 people.addNode(carol)
 people.printVals()
-people.insertNode(bob, alice, carol)
+people.insertNode(alice, carol)
+people.printVals()
+people.insertNode(bob, carol)
+people.printVals()
+people.deleteNode(bob)
 people.printVals()
 people.deleteNode(carol)
+people.printVals()
+people.deleteNode(alice)
 people.printVals()
